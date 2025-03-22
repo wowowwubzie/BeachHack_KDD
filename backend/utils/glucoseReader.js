fd1d057f561641619813a44de62628d264d6bdcd
katiemartinezz <119619811+katiemartinezz@users.noreply.github.com>
2025-03-21 18:30:59 -0700

    prediction

--- backend/utils/glucoseReader.js
+++ backend/utils/glucoseReader.js
@@ -5,18 +5,24 @@ const csv = require('csv-parser');
 // dataset is relatively small, so use an array to hold glucose readings for faster access
 const glucoseData = [];
 
-fs.createReadStream('../data/cleaned_glucose_data.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    const glucoseValue = parseFloat(row[' Scan_Glucose_mmol/L']);
-    
-    // store value with appropriate as appropriate type
-    glucoseData.push({
-      Timestamp_UTC: row['Timestamp_UTC'],
-      Scan_Glucose_mmol_L: glucoseValue,  // Use the parsed float value
-    });
-  })
-  .on('end', () => { // verification
-    console.log('cvs file successfully processed');
-   //console.log("First 5 records:", glucoseData.slice(0, 5)); // Log first 5 records 
+function loadGlucoseData() {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream('../data/cleaned_glucose_data.csv')
+      .pipe(csv())
+      .on('data', (row) => {
+        const glucoseValue = parseFloat(row[' Scan_Glucose_mmol/L']);
+        // store value with appropriate as appropriate type
+        glucoseData.push({
+          Timestamp_UTC: row['Timestamp_UTC'],
+          Scan_Glucose_mmol_L: glucoseValue, // the float value
+        });
+      })
+      .on('end', () => { // verification
+        console.log('csv file successfully processed');
+        resolve(glucoseData); // Resolve the promise with the data
+      })
+      .on('error', (error) => reject(error)); // Handle error
   });
+}
+// export glucoseData array to make it available in other files
+module.exports = loadGlucoseData;
