a5982d57bf33750d1366ce9409a436616579e23d
Deanna S <deannasolis2003@gmail.com>
2025-03-22 12:31:55 -0700

    Add new components and update package.json

---
+++ my-app/backend/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Import routes
+const nutritionRoute = require('./routes/nutrition');
+app.use('/nutrition', nutritionRoute);
+
+// Start server
+//const PORT = 5000;
+const PORT = process.env.PORT || 5001; // Change 5000 to 5001 if needed
+app.post("/nutrition", (req, res) => {
+    const barcode = req.body.barcode;
+    console.log("Received barcode from camera_test.py:", barcode); // Add this
+  
+  });
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
